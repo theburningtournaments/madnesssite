01e310b496c506edad092b983b0dd22b6bad7806
theburningtournaments <ccarls782@gmail.com>
2026-03-20 03:19:25 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDKwDmRaPgLUa2jB4MTq_OPAyehPnnVPO8",
+  authDomain: "madness-fe0a8.firebaseapp.com",
+  projectId: "madness-fe0a8",
+  storageBucket: "madness-fe0a8.firebasestorage.app",
+  messagingSenderId: "1043654768710",
+  appId: "1:1043654768710:web:91bf3c5d33aa6eac643247",
+  measurementId: "G-G0M19FBPZC"
+};
